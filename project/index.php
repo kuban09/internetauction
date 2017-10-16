a413513f3951ef71e72fe4d53887d7f153546096
kuban09 <32679511+kuban09@users.noreply.github.com>
2017-10-16 12:56:56 +0200

    Add files via upload
    Podstawowy kod + historyjki

---
+++ project/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $index = '
+        <!DOCTYPE html>
+        <html lang="pl_PL">
+            <head>
+                <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+            <body>
+                
+            </body>
+        </html>
+    ';
+
+    echo $index;
+?>
