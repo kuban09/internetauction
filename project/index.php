89f0ab62d2ace252e4e30b6a680d439250de92eb
kuban09 <32679511+kuban09@users.noreply.github.com>
2017-10-25 19:17:32 +0200

    Bootstrap + połączenie do MySQL

--- project/index.php
+++ project/index.php
@@ -1,5 +1,7 @@
 <?php
 
+    include "mysql.php";
+
     $index = '
         <!DOCTYPE html>
         <html lang="pl_PL">
@@ -7,6 +9,16 @@
                 <meta charset="utf-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
+                
+                <link href="css/bootstrap.min.css" rel="stylesheet">
+                <link href="css/main.css" rel="stylesheet">
+                
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+                <script src="js/bootstrap.min.js"></script>
+                <!--[if lt IE 9]>
+                    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+                    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+                <![endif]-->
             </head>
             <body>
                 
